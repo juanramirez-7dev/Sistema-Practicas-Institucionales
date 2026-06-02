c9c3b92cf5e3ee2a8f15113f12be7f7bf0845ceb
juan_ramirez <jjramirezvasquez2007@gmail.com>
2026-06-02 17:07:39 -0500

    Proceso Controller

---
+++ backend/Practicas.API/Controllers/ProcesoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practicas.API.DTOs.Proceso;
+using Practicas.Domain.Interfaces.Services;
+using System.Security.Claims;
+
+namespace Practicas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcesoController : ControllerBase
+    {
+        private readonly IProcesoService _procesoService;
+        protected Guid UsuarioId => Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+
+        public ProcesoController(IProcesoService procesoService)
+        {
+            _procesoService = procesoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ProcesoResponseDto>> GetByEstudiante()
+        {
+            try
+            {
+                var proceso = await _procesoService.GetByEstudianteIdAsync(UsuarioId);
+                return Ok(new ProcesoResponseDto
+                {
+                    Id = proceso.Id,
+                    Estado = proceso.Estado
+                });
+            }
+            catch( KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ backend/Practicas.API/DTOs/Proceso/ProcesoResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using Practicas.Domain.Enums;
+
+namespace Practicas.API.DTOs.Proceso
+{
+    public class ProcesoResponseDto
+    {
+        public Guid Id { get; set; }
+        public EstadoProceso Estado { get; set; }
+    }
+}
