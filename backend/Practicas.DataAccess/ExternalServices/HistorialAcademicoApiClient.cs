bb8de87a6ad7b55ed15ba4338f39f4ce89418879
JhonFranco <shandoworf@gmail.com>
2026-06-02 18:02:01 -0500

    Se agrego una excepcion para cuando se quieran validar creditos de un estudiante inexistente

--- backend/Practicas.DataAccess/ExternalServices/HistorialAcademicoApiClient.cs
+++ backend/Practicas.DataAccess/ExternalServices/HistorialAcademicoApiClient.cs
@@ -6,32 +6,39 @@ using System.Text.Json;
 
 namespace Practicas.DataAccess.ExternalServices
 {
+    using System.Net;
+
     public class HistorialAcademicoApiClient
     {
         private readonly HttpClient _httpClient;
 
-        public HistorialAcademicoApiClient(
-            HttpClient httpClient)
+        public HistorialAcademicoApiClient(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
         public async Task<List<ExternalHistorialResponse>> ObtenerHistorialAsync(string documento)
         {
-            var response = await _httpClient.GetAsync($"api/HistorialAcademico/Document/{documento}/HistorialAcademico");
+            var response = await _httpClient.GetAsync(
+                $"api/HistorialAcademico/Document/{documento}/HistorialAcademico");
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new KeyNotFoundException(
+                    $"No se encontro al estudiante con documento: {documento}");
+            }
 
             response.EnsureSuccessStatusCode();
 
-            var json =
-                await response.Content.ReadAsStringAsync();
+            var json = await response.Content.ReadAsStringAsync();
 
-            return JsonSerializer.Deserialize<
-                List<ExternalHistorialResponse>>(
-                    json,
-                    new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    })!;
+            return JsonSerializer.Deserialize<List<ExternalHistorialResponse>>(
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                })!;
         }
     }
 }
+
